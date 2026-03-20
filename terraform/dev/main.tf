c92f984d57d8d39c02fb4508e32286c276af48c3
tunjiadeshina <adetunji.adeshina@gmail.com>
2026-03-20 14:36:08 -0600

    CICD improvements

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -129,7 +129,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   ]
 
   admin_ssh_key {
-    username   = var.admin_username
+    username   = "azureuser"
     public_key = var.ssh_public_key 
   }
 
