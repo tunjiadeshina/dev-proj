4be88123b158e06410322ca525859ea40ae4e730
Adetunji Adeshina <59351722+tunjiadeshina@users.noreply.github.com>
2026-03-20 16:19:25 -0600

    Revert "Dev"

--- terraform/dev/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "ssh_public_key" {
-  description = "SSH public key for VM access"
-  type        = string
-  sensitive   = true
-}
--- terraform/monitoring/outputs.tf
+++
@@ -1,4 +0,0 @@
-# terraform/monitoring/outputs.tf
-output "monitoring_vm_public_ip" {
-  value = azurerm_public_ip.monitoring_ip.ip_address
-}
