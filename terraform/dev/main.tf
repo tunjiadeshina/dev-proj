897556aa2584ecbe83c99c25875f08aa237b2835
tunjiadeshina <adetunji.adeshina@gmail.com>
2026-03-20 22:30:17 -0600

    fix naming issues/errors

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -78,6 +78,20 @@ resource "azurerm_network_security_rule" "http3000" {
   network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+resource "azurerm_network_security_rule" "http9100" {
+  name                        = "allow-http9100"
+  priority                    = 1040  
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "9100"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
 resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
